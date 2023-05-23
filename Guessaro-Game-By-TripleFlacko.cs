e977971d15aab1249487d8b93cdfcbfae93794e9
TripleFlacko <3rundark01@gmail.com>
2023-05-23 17:46:05 +0300

    Added features, colors and difficulty to the level.
    Added:
    
    - 10 tries
    - Play again
    - Colors

--- Guessaro-Game-By-TripleFlacko.cs
+++ Guessaro-Game-By-TripleFlacko.cs
@@ -1,41 +1,106 @@
 ﻿using System;
 
-class GuessANumberByTripleFlacko
+class GuessaroByTripleFlacko
 {
     static void Main()
     {
-        Random randomNumber = new Random();
-        int computerNumber = randomNumber.Next(1, 101);
+        int triesCounter = 10;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Guessaro is a Guess A Number Game created by TripleFlacko.\nYou'll have 10 tries to guess the number.");
+        Console.WriteLine();
+        bool loopBreak = false;
 
         while (true)
         {
-            Console.Write("Guess a number from 1 to 100: ");
-            string playerInput = Console.ReadLine();
-            bool isValid = int.TryParse(playerInput, out int playerNumber);
+            Random randomNumber = new Random();
+            int computerNumber = randomNumber.Next(1, 101);
 
-            if (isValid)
+            while (true)
             {
-                if (playerNumber == computerNumber)
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Guess a number from 1 to 100: ");
+                string playerInput = Console.ReadLine();
+                bool isValid = int.TryParse(playerInput, out int playerNumber);
+
+                if (isValid)
                 {
-                    Console.WriteLine("You guessed it!");
-                    Console.WriteLine();
-                    break;
-                }
-                else if (playerNumber < computerNumber)
-                {
-                    Console.WriteLine("Too Low");
-                    Console.WriteLine();
+                    if (playerNumber == computerNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Congratulations! You guessed it!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine();
+                        Console.WriteLine(@"Type ""Y"" to play again or type ""N"" to quit the game:");
+                        while (true)
+                        {
+                            string continueOrNot = Console.ReadLine().ToLower();
+                            if (continueOrNot == "y" || continueOrNot == "yes")
+                            {
+                                triesCounter = 10;
+                                loopBreak = true;
+                                Console.Clear();
+                                break;
+                            }
+                            else if (continueOrNot == "n" || continueOrNot == "no")
+                            {
+                                return;
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Invalid input. Please try again...");
+                            }
+                        }
+                    }
+                    else if (playerNumber < computerNumber)
+                    {
+                        triesCounter--;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Too Low\nYou have {triesCounter} tries left.");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        triesCounter--;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Too High\nYou have {triesCounter} tries left.");
+                        Console.WriteLine();
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Too High");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input. Try again...");
                     Console.WriteLine();
                 }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Try again...");
-                Console.WriteLine();
+                if (loopBreak)
+                {
+                    loopBreak = false;
+                    break;
+                }
+                if (triesCounter == 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Unfortunately, you lost. Do you want to try again?");
+                    Console.WriteLine(@"Type ""Y"" to try again or type ""N"" to quit the game:");
+                    while (true)
+                    {
+                        string continueOrNot = Console.ReadLine().ToLower();
+                        if (continueOrNot == "y" || continueOrNot == "yes")
+                        {
+                            triesCounter = 10;
+                            break;
+                        }
+                        else if (continueOrNot == "n" || continueOrNot == "no")
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. Please try again...");
+                        }
+                    }
+                }
             }
         }
     }
